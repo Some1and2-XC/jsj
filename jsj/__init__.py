381c086115598e058336b8375ac18a2b81e81edf
MT <04x0xx@gmail.com>
2024-07-02 20:10:17 -0300

    feat: Updated example and added flatten method

--- jsj/__init__.py
+++ jsj/__init__.py
@@ -1,4 +1,22 @@
-import asyncio
+"""
+JSJ (JS-JSON)
+---
+JSJ is a python library aimed at getting the JavaScript Experience of working with API's.
+Specifically, making data be accessible through dot notation and having a built-in way of flattening JSON values.
+
+Basic Usage:
+    from jsj import *
+
+    url = "https://api.weather.gov/points/39.7632,-101.6483"
+
+    time_zone = fetch(url) \
+        .json() \
+        .then(lambda v: v.properies.timeZone) \
+        .get_data()
+
+    assert time_zone == "America/Chicago"
+"""
+
 import json
 import requests
 
@@ -10,9 +28,42 @@ class JSON(dict):
     JSON: a wrapper class for the default dictionary.
     Wrapping the default class allows for using dot notation to get values.
     """
-    __getattr__ = dict.get
+    def __getattr__(self, key):
+        if type(self[key]) is dict:
+            return JSON(self[key])
+        else:
+            return self[key]
     __setattr__ = dict.__setitem__
     __delattr__ = dict.__delitem__
+
+    def flatten(self, base: list = []) -> dict:
+        """
+        Function for flattening data, inspired by the pandas `pd.json_normalize()` function.
+        """
+
+        def recurs_flat(item, index="") -> dict:
+            out = dict()
+
+            if type(item) is dict or type(item) is JSON:
+                for k in item:
+                    out |= recurs_flat(item[k], index + k + "_")
+
+            elif type(item) is list:
+                for i, v in enumerate(item):
+                    out |= recurs_flat(v, index + str(i) + "_")
+
+            else:
+                out[index[:-1]] = item # Does fancy name indexing to remove '_'
+
+            return out
+
+        out_lst: dict | list = self
+        for k in base:
+            out_lst = out_lst[k]
+
+        assert type(out_lst) is list
+
+        return recurs_flat(out_lst)
 
 
 class Data:
@@ -23,11 +74,16 @@ class Data:
     def __init__(self, data):
         self.data = data
 
+
     def then(self, callback: Callable) -> Self:
-        """
-        Calls a callback and returns a `Data` object holding the response.
-        """
+        """Calls a callback and returns a `Data` object holding the response."""
         return Data(callback(self.data))
+
+
+    def get_data(self) -> Any:
+        """Returns the internal data of the data class."""
+        return self.data
+
 
     def __repr__(self) -> str:
         return str(self.data)
@@ -40,6 +96,7 @@ class Response(Data):
     """
     def __init__(self, res: requests.Response):
         super().__init__(res)
+
 
     def json(self) -> Data(JSON[Any]):
         """
@@ -58,11 +115,12 @@ def fetch(url: str, params: dict = {}) -> Response:
 
 
 if __name__ == "__main__":
-    url = "https://dog.ceo/api/breeds/image/random"
+    url = "https://api.weather.gov/points/39.7632,-101.6483"
 
-    v = fetch(url) \
+    time_zone = fetch(url) \
         .json() \
-        .then(lambda v: v.message)
+        .then(lambda v: v.properties.timeZone) \
+        .get_data()
 
-    print(v.data)
-
+    print(time_zone)
+    assert time_zone == "America/Chicago"
