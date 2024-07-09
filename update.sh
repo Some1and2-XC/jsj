5490abfd7fa16c401d4f8582e31ec41f84ce3467
Mark <04x0xx@gmail.com>
2024-07-09 16:21:19 -0300

    patch: Changed python version in `update.sh` script

--- update.sh
+++ update.sh
@@ -1,17 +1,19 @@
-#!/usr/bin/env bash
+# !/bin/bash
 
 # Script for automatically pushing updates to PyPi
 
-python -m unittest
+source ./venv/bin/activate
+
+python3 -m unittest
 
 if [[ $? -ne 0 ]]; then
     read -p ""
     exit 1
 fi
 
-python -m build
-python -m twine upload --repository pypi dist/*
-python -m pip install --upgrade jsj
+python3 -m build
+python3 -m twine upload --repository pypi dist/*
+python3 -m pip install --upgrade jsj
 rm -rf dist
 
 read -p "Deployment completed Successfully! Press [enter] to finish..."
